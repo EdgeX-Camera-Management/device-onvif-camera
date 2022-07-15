44e279d1a7c76ce72226a77f7ed7009dfa54f027
Darr, Christian <christian.darr@intel.com>
2022-07-15 14:45:35 -0500

    test: set tests
    Signed-off-by: Darr, Christian <christian.darr@intel.com>

--- internal/driver/onvifdiscovery_test.go
+++ internal/driver/onvifdiscovery_test.go
@@ -50,39 +50,125 @@ func createTestDeviceList() []contract.Device {
 }
 
 func TestOnvifDiscovery_makeDeviceMap(t *testing.T) {
-	mockService, driver := createDriverWithMockService()
-	mockService.On("Devices").
-		Return(createTestDeviceList()).Once()
-	mockService.On("Name").
-		Return("device-onvif-camera").Times(4)
-	devices := driver.makeDeviceMap()
-	mockService.AssertExpectations(t)
+	tests := []struct {
+		name      string
+		devices   []contract.Device
+		deviceMap map[string]contract.Device
+		nameCalls int
+	}{
+		{
+			name:    "3 devices",
+			devices: createTestDeviceList(),
+			deviceMap: map[string]contract.Device{
+				"123": {
+					Name: "testDevice1", Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "123",
+						},
+					},
+				},
+				"456": {
+					Name: "testDevice2", Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "456",
+						},
+					},
+				},
+				"789": {
+					Name: "testDevice3", Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "789",
+						},
+					},
+				},
+			},
+			nameCalls: 4,
+		},
+		{
+			name: "NoProtocol",
+			devices: []contract.Device{
+				{
+					Name: "testDevice1",
+					Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "123",
+						},
+					},
+				},
+				{
+					Name:      "testDevice2",
+					Protocols: map[string]models.ProtocolProperties{},
+				},
+			},
+			deviceMap: map[string]contract.Device{
+				"123": {
+					Name: "testDevice1", Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "123",
+						},
+					},
+				},
+			},
+			nameCalls: 2,
+		},
+		{
+			name: "NoEndpointReference",
+			devices: []contract.Device{
+				{
+					Name: "testDevice1",
+					Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "123",
+						},
+					},
+				},
+				{
+					Name: "testDevice2",
+					Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "",
+						},
+					},
+				},
+			},
+			deviceMap: map[string]contract.Device{
+				"123": {
+					Name: "testDevice1", Protocols: map[string]models.ProtocolProperties{
+						OnvifProtocol: map[string]string{
+							EndpointRefAddress: "123",
+						},
+					},
+				},
+			},
+			nameCalls: 2,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			mockService, driver := createDriverWithMockService()
+			mockService.On("Devices").
+				Return(test.devices).Once()
+			mockService.On("Name").
+				Return("device-onvif-camera").Times(test.nameCalls)
+			devices := driver.makeDeviceMap()
+			mockService.AssertExpectations(t)
 
-	assert.Equal(t, devices, map[string]contract.Device{
-		"123": {
-			Name: "testDevice1", Protocols: map[string]models.ProtocolProperties{
-				OnvifProtocol: map[string]string{
-					EndpointRefAddress: "123",
-				},
-			},
-		},
-		"456": {
-			Name: "testDevice2", Protocols: map[string]models.ProtocolProperties{
-				OnvifProtocol: map[string]string{
-					EndpointRefAddress: "456",
-				},
-			},
-		},
-		"789": {
-			Name: "testDevice3", Protocols: map[string]models.ProtocolProperties{
-				OnvifProtocol: map[string]string{
-					EndpointRefAddress: "789",
-				},
-			},
-		},
-	})
+			assert.Equal(t, devices, test.deviceMap)
+		})
+	}
 }
 
+// func TestOnvifDiscovery_discoveryFilter(t *testing.T) {
+// 	tests := []struct {
+// 		{
+// 			name:
+// 			discoveredDevices []
+// 		}
+// 	}{
+
+// 	}
+// }
 func TestOnvifDiscovery_discoverFilter_noNewDevices(t *testing.T) {
 	mockService, driver := createDriverWithMockService()
 	mockService.On("Devices").
@@ -239,3 +325,35 @@ func TestOnvifDiscovery_discoverFilter_mixedNewAndOldDevices(t *testing.T) {
 		},
 	})
 }
+
+// func TestOnvifDiscovery_updateExistingDevice(t *testing.T) {
+// 	mockService, driver := createDriverWithMockService()
+// 	mockService.On("UpdateDevice", models.Device{
+// 		Protocols: map[string]contract.ProtocolProperties{
+// 			OnvifProtocol: map[string]string{
+// 				"Address":  "5.6.7.8",
+// 				"Port":     "2",
+// 				"LastSeen": time.Now().Format(time.UnixDate),
+// 			},
+// 		},
+// 	}).Return(nil).Once()
+// 	err := driver.updateExistingDevice(
+// 		contract.Device{
+// 			Protocols: map[string]contract.ProtocolProperties{
+// 				OnvifProtocol: map[string]string{
+// 					"Address": "1.2.3.4",
+// 					"Port":    "1",
+// 				},
+// 			},
+// 		}, sdkModel.DiscoveredDevice{
+// 			Protocols: map[string]contract.ProtocolProperties{
+// 				OnvifProtocol: map[string]string{
+// 					"Address": "5.6.7.8",
+// 					"Port":    "2",
+// 				},
+// 			},
+// 		})
+// 	driver.lc.Info("Helo")
+// 	mockService.AssertExpectations(t)
+// 	require.NoError(t, err)
+// }
